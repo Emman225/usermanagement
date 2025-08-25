ccfc20cf5c04306ee7e922a7adeedf4c194da4ac
Emman225 <56003477+Emman225@users.noreply.github.com>
2025-08-25 00:51:49 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,17 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'fonts.gstatic.com',
+        protocol: "https",
+        hostname: "fonts.gstatic.com",
       },
       {
-        protocol: 'https',
-        hostname: 'usermanagement-api.ecoletestpro.com',
+        protocol: "https",
+        hostname: "usermanagement-api.ecoletestpro.com",
+        pathname: "/storage/**", // ✅ autorise tous les fichiers dans /storage/
       },
     ],
   },
 };
 
 module.exports = nextConfig;
+
